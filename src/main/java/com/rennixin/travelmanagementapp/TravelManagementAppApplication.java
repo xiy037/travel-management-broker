edabd9df76573b02e97957a09ceead4ab308425a
Xiao Yu <xiao.yu@thoughtworks.com>
2022-06-11 17:23:04 +0800

    init project

---
+++ src/main/java/com/rennixin/travelmanagementapp/TravelManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.rennixin.travelmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rennixin/travelmanagementapp/TravelManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rennixin.travelmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
